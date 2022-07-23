623b970ed40f808563bec0842b7cd9257104ec7c
raghuram <raghurammailbox@gmail.com>
2022-07-23 20:24:01 +0530

    23 Jun

---
+++ files2Video.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+import sys
+import traceback # for debugging
+import numpy as np
+import csv
+
+
+from ai_component import *
+from get_bb import *
+
+# define thres in one place
+def our_thres(img):
+    return cv2.adaptiveThreshold(img,1,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,15,3)
+
+
+
+image_folder =  "/home/lavan/ALL_IN_ONE_RGB_IMG_ANOT"
+video_name = 'orginal.mp4'
+
+f = [str(i).zfill(3) for i in range(1,1730)]
+
+images = [n+".jpg" for n in f ]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*"mp4v"), 30, (width,height), True)
+#video_org = cv2.VideoWriter("original.avi", 0, 30, (width,height),False)
+
+#imageStarsCropped = cv2.bitwise_and(imageStars, mask)
+for image in images:
+    print(f"processing {image} ")
+    f = os.path.join(image_folder, image)
+    if(  os.path.isfile(f) == False ):
+        print(f"warn * {f} not found")
+        continue
+    frame = cv2.imread( f, cv2.IMREAD_GRAYSCALE)
+
+    
+    
+    try:
+        video.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    except:
+        err = sys.exc_info()
+        print(f"{traceback.print_tb(err[1])} \n {err[0]}")
+        
+        
+
+
+cv2.destroyAllWindows()
+video.release()
+print("Videowriten")
